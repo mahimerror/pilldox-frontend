d30dbd149a9c74a93554e9564188d1025a551bc3
Sajnin14 <sajninsaima@gmail.com>
2025-05-17 09:42:40 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        normalFont: ["normalFont"],
+        poppins: ["'Poppins', 'sans-serif'"],
+        mediumFont: ["mediumFont"],
+        boldFont: ["boldFont"],
+      },
+      colors: {
+        primary: "#008080",
+        textColor: "#222222",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
