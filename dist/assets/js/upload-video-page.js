1154da86cb61e566bb5717b39d8cb38093e9e1e8
Zobayer <zobayerhosen2345@gmail.com>
2025-05-19 16:41:03 +0600

    change some style & function

--- dist/assets/js/upload-video-page.js
+++ dist/assets/js/upload-video-page.js
@@ -1,34 +1,52 @@
-// zh modal content show selector start
-const showModalBtn = document.getElementById("showModalBtn");
-const overlay = document.getElementById("modalOverlay");
-const modalContent = document.getElementById("modalContent");
+// zh form validation start
+const form = document.getElementById("upload-form");
 
-//   show the modal
-function showModal() {
-  overlay.classList.remove("hidden");
-}
-//   hide the modal
-function hideModal() {
-  overlay.classList.add("hidden");
-}
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formTitle = document.getElementById("add-title").value;
+  const formDescription = document.getElementById("add-description").value;
+  const formTags = document.getElementById("add-tags").value;
 
-//   add listeners
-showModalBtn.addEventListener("click", showModal);
-overlay.addEventListener("click", function (event) {
-  if (!modalContent.contains(event.target)) {
-    hideModal();
+  // validation
+  if (!formTitle || !formDescription || !formTags) {
+    alert("Please fill in the all field");
+    return;
   }
-  console.log(overlay);
-});
-console.log(overlay);
-// zh modal content show selector end
 
-// zh video upload and show video name and video size
+  // zh modal content show selector start
+  const showModalBtn = document.getElementById("showModalBtn");
+  const overlay = document.getElementById("modalOverlay");
+  const modalContent = document.getElementById("modalContent");
+
+  //   show the modal
+  const showModal = () => {
+    overlay.classList.remove("hidden");
+  };
+  //   hide the modal
+  const hideModal = () => {
+    overlay.classList.add("hidden");
+  };
+
+  //   add listeners
+  showModalBtn.addEventListener("click", showModal);
+  overlay.addEventListener("click", function (event) {
+    if (!modalContent.contains(event.target)) {
+      hideModal();
+    }
+    console.log(overlay);
+  });
+  console.log(overlay);
+  // zh modal content show selector end
+});
+// zh form validation end
+
+// zh video upload and show video name and video size start
 const input = document.getElementById("roomsCoverUpload");
 const videoInfo = document.getElementById("vide-info");
 const videoName = document.getElementById("upload-video-name");
 const videoSize = document.getElementById("upload-video-size");
 const videoBoxCloseBtn = document.getElementById("video-preview-close-btn");
+const videTitle = document.getElementById("video-title");
 
 input.addEventListener("change", () => {
   const file = input.files[0];
@@ -37,6 +55,7 @@ input.addEventListener("change", () => {
   if (file) {
     videoInfo.classList.remove("hidden");
     videoName.textContent = `${file.name}`;
+    videTitle.textContent = `${file.name}`;
 
     const sizeMb = file.size / (1024 * 1024);
     console.log(sizeMb);
@@ -54,4 +73,4 @@ input.addEventListener("change", () => {
 videoBoxCloseBtn.addEventListener("click", () => {
   videoInfo.classList.add("hidden");
 });
-// zh video upload and show video name and video size
+// zh video upload and show video name and video size end
